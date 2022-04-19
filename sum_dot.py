30dab398912da3593c7557c586f62fffc35f707f
farid <faridhavasi@yahoo.com>
2022-04-19 18:23:20 +0430

    s

--- sum_dot.py
+++ sum_dot.py
@@ -1,31 +1,19 @@
-def sum_no(n:str):
+def shpmr(n:str):    
+    conter=0
     yek_n=['ز','ظ','ذ','ب','ن','ف','ض','خ','ج','غ']
-    doo_n=['ت','ق','ِِِِِِِِی']
-    seh_n=['چ','پ','ژ','ث','ش']
-    content_yek=0
-    content_doo=0
-    content_seh=0
-    for i in n:
-        if i not in yek_n and i not in doo_n and i not in seh_n:
-            pass
-        elif i in yek_n:
-            yek=+1
-            content_yek=+yek
-            #print(i)
-       
-            print('yek',yek)
-        elif i in doo_n:
-            doo=+2
-            content_doo=+doo
-            #print(i)
-      
-            print('do:',doo)
-        elif i in seh_n:
-            seh=+3
-            content_seh=+seh
-            #print(i)
-    
-            print('seh:',seh)
-        content_total=content_yek+content_doo+content_seh
-    
-    return (content_total)     
+    doo_n=['ت','ق','ی']
+    seh_n=['چ','‍‍‍‍‍‍‍‍‍‍‍پ','ث','ش']
+
+    for char in n.split():
+        for i in char:
+            if i in yek_n:
+                conter=conter+1
+                print(i)
+            elif i in doo_n:
+                conter=conter+2
+                print(i)
+            elif i in seh_n:
+                conter=conter+3
+                print(i)    
+    print(conter)
+shpmr('خدای مهربان')
