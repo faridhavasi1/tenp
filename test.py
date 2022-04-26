10560c2cdba6bb31ac2e43b9ecaefd938b574156
farid <faridhavasi@yahoo.com>
2022-04-26 19:23:27 +0430

    task

---
+++ test.py
@@ -0,0 +1,16 @@
+
+def getPrefixSuffix(a, b, l):
+	prefix = a[: l]
+	lb = len(b)
+	suffix = b[lb - l:]
+
+	return (prefix + suffix)
+
+a = 'ali'
+b = 'lia'
+l = 5
+print(getPrefixSuffix(a, b, l))
+
+
+
+
